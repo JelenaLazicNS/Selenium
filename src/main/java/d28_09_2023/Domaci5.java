ace168d899900f47c00c9829064ab4558ab632b3
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-01 11:24:25 +0200

    Selenium Wait homework

---
+++ src/main/java/d28_09_2023/Domaci5.java
@@ -0,0 +1,33 @@
+package d28_09_2023;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Domaci5 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("http://seleniumdemo.com/?product=bdd-cucumber");
+        driver.findElement(By.xpath("//*[@id=\"menu-item-20\"]/a")).click();
+        wait.until(ExpectedConditions.titleIs("Cart – Selenium Demo Page"));
+        WebElement cartEmpty = null;
+        try{
+            cartEmpty = driver.findElement(By.cssSelector(".cart-empty"));
+        }catch (Exception e){
+        }
+        if(cartEmpty != null) {
+            System.out.println("Cart is empty");
+        }
+        else System.out.println("There are products in the cart");
+        driver.quit();
+    }
+}
