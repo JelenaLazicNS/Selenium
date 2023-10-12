8a56193273a3520f5f5037015079f76665a9197f
Jelena Lazic <jelena.lazic.novi.sad@gmail.com>
2023-10-12 17:38:41 +0200

    Verify quantity of item

--- src/test/java/tests/SwagLabsTest.java
+++ src/test/java/tests/SwagLabsTest.java
@@ -325,6 +325,20 @@ public class SwagLabsTest extends BasicTest {
                         itemList.getItemsDescription(0)));
 
     }
+    @Test
+    public void verifyIfTheQuantityOfItemIsPresented(){
+        login.clearAndTypeUsername(username);
+        login.clearAndTypePassword(password);
+        login.clickOnLoginButton();
+
+        inventory.clickOnAddToCartButton();
+        topNav.clickOnCartButton();
+
+        wait
+                .withMessage("Item's quantity should be visible.")
+                .until(ExpectedConditions.textToBePresentInElement(itemList.getSingleItem(0),
+                        itemList.getItemsQuantity(0)));
+    }
 
 
 
